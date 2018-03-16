980562502d8a6bd82feb8bb7008c817f1bcd7064
Grantas Gadliauskas <grantas0612@gmail.com>
2018-03-16 23:24:12 +0200

    update autoloader

--- autoloader.php
+++ autoloader.php
@@ -6,8 +6,24 @@
  * Time: 15.38
  */
 
+
 function autoload($className)
 {
+    $CONFIG_FILE = "autoloadConfig.json";
+    /*Šis failas atlieka tokią pat funkciją kaip composer.json
+    autoload psr-4 objektas -
+    "autoload": {
+    "psr-4": {
+
+    }
+    */
+    $maps = json_decode(file_get_contents($CONFIG_FILE));
+
+    foreach($maps as $key => $value){
+        if(substr($className, 0, strlen($key)) == $key){
+           $className =  $value.substr($className, strlen($key));
+        }
+}
     $file = str_replace('\\', DIRECTORY_SEPARATOR, $className.'.php');
     if (file_exists($file)){
          require $file;
